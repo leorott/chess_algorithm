92c4832e5db5b985985d31a048da96b2cc6dabd9
Leo Roettele <leo.roettele@liip.ch>
2023-01-09 08:13:12 +0100

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Let'git remote add origin git@github.com:leorott/chess_algorithm.gits play Chess");
+    }
+}
