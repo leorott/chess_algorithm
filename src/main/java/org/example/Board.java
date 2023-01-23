b58681a98ade493615cafaa788326913ef74b335
Leo Roettele <leo.roettele@liip.ch>
2023-01-23 07:45:44 +0100

    add comments

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,19 +12,24 @@ public class Board {
         String[] fenParts = fen.split(" ");
         String[] rows = fenParts[0].split("/");
 
+        // Iterate through the rows
         for (int i = 0; i < 8; i++) {
             int col = 0;
+            // Iterate through the characters in the row
             for (int j = 0; j < rows[i].length(); j++) {
                 char c = rows[i].charAt(j);
+                // If it is a digit, it represents the number of empty squares
                 if (Character.isDigit(c)) {
                     int numEmptySquares = Character.getNumericValue(c);
                     for (int k = 0; k < numEmptySquares; k++) {
                         String key = (char)('a' + col) + Integer.toString(8 - i);
+                        // Add the empty squares to the positions map
                         positions.put(key, "");
                         col++;
                     }
                 } else {
                     String key = (char)('a' + col) + Integer.toString(8 - i);
+                    // Add the piece to the positions map
                     positions.put(key, Character.toString(c));
                     col++;
                 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        String initialPositions = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR";
+        String initialPositions = "r1bqk1nr/ppppbppp/8/4N3/6PP/8/PPPPP3/RNBQKB1R w - - 10 18";
         Board board = new Board(initialPositions);
         Map<String, String> positions = board.getPositions();
         System.out.println(positions);
