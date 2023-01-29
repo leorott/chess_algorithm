7c939220f69bfc7be9cfdbc557deea711d5bd7f6
Leo Roettele <leo.roettele@liip.ch>
2023-01-30 00:11:15 +0100

    WIP: implement checkmate in 2 moves

--- src/main/java/ch/lucaleo/Engine.java
+++ src/main/java/ch/lucaleo/Engine.java
@@ -11,10 +11,33 @@ public class Engine {
     public Engine() {
     }
 
-    public boolean isCheckmateIn2MovesPossible(Stellung stellung){
+    public boolean isCheckmateIn2MovesPossible(Stellung initialStellung){
         Spielregeln spielregeln = new DefaultSpielregeln();
-        Collection<Zug> possibleMoves = spielregeln.liefereGueltigeZuege(stellung);
-        possibleMoves.forEach(System.out::println);
+        Collection<Zug> possibleMoves = spielregeln.liefereGueltigeZuege(initialStellung);
+        possibleMoves.forEach((zug1) -> {
+            Stellung stellungNachZug1 = initialStellung.fuehreZugAus(zug1);
+            System.out.println("Nach Zug 1:" + spielregeln.aufMattPruefen(stellungNachZug1));
+
+            Collection<Zug> possibleMovesAfterZug1 = spielregeln.liefereGueltigeZuege(stellungNachZug1);
+
+            possibleMovesAfterZug1.forEach((zug2) -> {
+                Stellung stellungNachZug2 = stellungNachZug1.fuehreZugAus(zug2);
+                System.out.println("Nach Zug 2:" + spielregeln.aufMattPruefen(stellungNachZug2));
+
+                Collection<Zug> possibleMovesAfterZug2 = spielregeln.liefereGueltigeZuege(stellungNachZug2);
+                possibleMovesAfterZug2.forEach((zug3) -> {
+                    Stellung stellungNachZug3 = stellungNachZug2.fuehreZugAus(zug3);
+                    System.out.println("Nach Zug 3: " + spielregeln.aufMattPruefen(stellungNachZug3));
+
+                    Collection<Zug> possibleMovesAfterZug3 = spielregeln.liefereGueltigeZuege(stellungNachZug3);
+                    possibleMovesAfterZug3.forEach((zug4) -> {
+                        Stellung stellungNachZug4 = stellungNachZug3.fuehreZugAus(zug4);
+                        System.out.println("Nach Zug 4: " + spielregeln.aufMattPruefen(stellungNachZug4));
+                    });
+                });
+
+            });
+        });
         return false;
     }
 }
