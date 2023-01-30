36152f6618a3893e03712c155963b3eb9c20ad3d
Leo Roettele <leo.roettele@liip.ch>
2023-01-30 21:20:08 +0100

    add some comments

--- src/main/java/ch/lucaleo/Engine.java
+++ src/main/java/ch/lucaleo/Engine.java
@@ -15,38 +15,43 @@ public class Engine {
 
     }
 
-
     public List<List<Zug>> isCheckmateIn2MovesPossible(Stellung initialStellung){
         Spielregeln spielregeln = new DefaultSpielregeln();
-        Collection<Zug> moeglicheZuege = spielregeln.liefereGueltigeZuege(initialStellung);
         List<List<Zug>> mattZuege = new ArrayList<>();
         AtomicReference<Integer> index = new AtomicReference<>(0);
+        Collection<Zug> moeglicheZuege = spielregeln.liefereGueltigeZuege(initialStellung);
 
+        // Iteration ueber alle erlaubten Zuege der Ausgangslage
         moeglicheZuege.forEach((teilzug1) -> {
             index.getAndSet(index.get() + 1);
             Stellung stellungNachTeilzug1 = initialStellung.fuehreZugAus(teilzug1);
             Collection<Zug> possibleMovesAfterZug1 = spielregeln.liefereGueltigeZuege(stellungNachTeilzug1);
 
+            // Iteration ueber alle erlaubten Zuege nach Teilzug 1
             possibleMovesAfterZug1.forEach((teilzug2) -> {
                 index.getAndSet(index.get() + 1);
                 Stellung stellungNachTeilzug2 = stellungNachTeilzug1.fuehreZugAus(teilzug2);
                 Collection<Zug> possibleMovesAfterZug2 = spielregeln.liefereGueltigeZuege(stellungNachTeilzug2);
 
+                // Iteration ueber alle erlaubten Zuege nach Teilzug 2
                 possibleMovesAfterZug2.forEach((teilzug3) -> {
                     index.getAndSet(index.get() + 1);
                     Stellung stellungNachTeilzug3 = stellungNachTeilzug2.fuehreZugAus(teilzug3);
                     Collection<Zug> possibleMovesAfterZug3 = spielregeln.liefereGueltigeZuege(stellungNachTeilzug3);
 
+                    // Iteration ueber alle erlaubten Zuege nach Teilzug 3
                     possibleMovesAfterZug3.forEach((teilzug4) -> {
                         index.getAndSet(index.get() + 1);
                         Stellung stellungNachTeilzug4 = stellungNachTeilzug3.fuehreZugAus(teilzug4);
 
+                        // Ueberpruefen ob Matt moeglich ist
                         if (spielregeln.aufMattPruefen(stellungNachTeilzug4)){
                             List<Zug> zuege = new ArrayList<>();
                             zuege.add(teilzug1);
                             zuege.add(teilzug2);
                             zuege.add(teilzug3);
                             zuege.add(teilzug4);
+                            // Alle Teilzuege zu liste hinzufuegen
                             mattZuege.add(zuege);
                         }
                     });
@@ -57,5 +62,4 @@ public class Engine {
         System.out.println("Anzahl Iterationen: " + index);
         return mattZuege;
     }
-
 }
--- src/main/java/ch/lucaleo/Main.java
+++ src/main/java/ch/lucaleo/Main.java
@@ -7,15 +7,19 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Ausgangslage
         String fen = "r1bqk1nr/ppppbppp/8/4N3/6PP/8/PPPPP3/RNBQKB1R w - - 10 18";
         Stellung stellung = new Stellung(fen);
         Engine engine = new Engine();
+
+        // Liste aller moeglichen Zugskombinationen mit denen Matt erreicht werden kann
         List<List<Zug>> mattZuege = engine.isCheckmateIn2MovesPossible(stellung);
 
         FileHandler fileHandler = new FileHandler();
         String fileName = fileHandler.createFile("Output.md");
 
         if (mattZuege.size() >= 1) {
+            // Matt in 2 Zuegen ist moeglich
             String fileContent = "# Output\n\n Ausgehend von folgendem Spielstand: **\""
                     + fen + "\"** ist Schachmatt in 2 Zügen auf folgende Arten möglich: \n \n";
 
@@ -25,8 +29,8 @@ public class Main {
 
             fileHandler.writeToFile(fileName, fileContent);
         } else {
+            // Matt in 2 Zuegen ist nicht moeglich
             fileHandler.writeToFile(fileName, "Matt in 2 Zügen ist nicht möglich");
-
         }
     }
 }
