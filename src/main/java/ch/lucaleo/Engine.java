b8aa83eb0ad750f61aa3a4ff91076ed36fdf8ba6
Leo Roettele <leo.roettele@liip.ch>
2023-01-30 14:19:01 +0100

    add better output

--- src/main/java/ch/lucaleo/Engine.java
+++ src/main/java/ch/lucaleo/Engine.java
@@ -5,39 +5,43 @@ import de.dokchess.allgemein.Zug;
 import de.dokchess.regeln.DefaultSpielregeln;
 import de.dokchess.regeln.Spielregeln;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Engine {
     public Engine() {
     }
 
-    public boolean isCheckmateIn2MovesPossible(Stellung initialStellung){
+    public List<Zug> isCheckmateIn2MovesPossible(Stellung initialStellung){
         Spielregeln spielregeln = new DefaultSpielregeln();
         Collection<Zug> possibleMoves = spielregeln.liefereGueltigeZuege(initialStellung);
+        List<Zug> mattZuege = new ArrayList<Zug>();
         possibleMoves.forEach((zug1) -> {
             Stellung stellungNachZug1 = initialStellung.fuehreZugAus(zug1);
-            System.out.println("Nach Zug 1:" + spielregeln.aufMattPruefen(stellungNachZug1));
 
             Collection<Zug> possibleMovesAfterZug1 = spielregeln.liefereGueltigeZuege(stellungNachZug1);
 
             possibleMovesAfterZug1.forEach((zug2) -> {
                 Stellung stellungNachZug2 = stellungNachZug1.fuehreZugAus(zug2);
-                System.out.println("Nach Zug 2:" + spielregeln.aufMattPruefen(stellungNachZug2));
 
                 Collection<Zug> possibleMovesAfterZug2 = spielregeln.liefereGueltigeZuege(stellungNachZug2);
                 possibleMovesAfterZug2.forEach((zug3) -> {
                     Stellung stellungNachZug3 = stellungNachZug2.fuehreZugAus(zug3);
-                    System.out.println("Nach Zug 3: " + spielregeln.aufMattPruefen(stellungNachZug3));
 
                     Collection<Zug> possibleMovesAfterZug3 = spielregeln.liefereGueltigeZuege(stellungNachZug3);
                     possibleMovesAfterZug3.forEach((zug4) -> {
                         Stellung stellungNachZug4 = stellungNachZug3.fuehreZugAus(zug4);
-                        System.out.println("Nach Zug 4: " + spielregeln.aufMattPruefen(stellungNachZug4));
+                        if (spielregeln.aufMattPruefen(stellungNachZug4)){
+                            System.out.println("schach!");
+                            mattZuege.add(zug4);
+
+                        }
                     });
                 });
 
             });
         });
-        return false;
+        return mattZuege;
     }
 }
--- src/main/java/ch/lucaleo/Main.java
+++ src/main/java/ch/lucaleo/Main.java
@@ -1,8 +1,8 @@
 package ch.lucaleo;
 
 import de.dokchess.allgemein.Stellung;
-import de.dokchess.regeln.DefaultSpielregeln;
-import de.dokchess.regeln.Spielregeln;
+import de.dokchess.allgemein.Zug;
+import java.util.List;
 
 
 public class Main {
@@ -11,9 +11,15 @@ public class Main {
         String initialPositions = "r1bqk1nr/ppppbppp/8/4N3/6PP/8/PPPPP3/RNBQKB1R w - - 10 18";
 */
         Stellung stellung = new Stellung("r1bqk1nr/ppppbppp/8/4N3/6PP/8/PPPPP3/RNBQKB1R w - - 10 18");
-        Spielregeln spielregeln = new DefaultSpielregeln();
-        System.out.println(spielregeln.liefereGueltigeZuege(stellung));
         Engine engine = new Engine();
-        engine.isCheckmateIn2MovesPossible(stellung);
+        List<Zug> mattZuege = engine.isCheckmateIn2MovesPossible(stellung);
+
+        if (mattZuege.size() >= 1){
+            System.out.println("Matt in zwei Zuegen ist moeglich");
+            mattZuege.forEach(System.out::println);
+
+        } else {
+            System.out.println("Matt ist nicht möglich");
+        }
     }
 }
